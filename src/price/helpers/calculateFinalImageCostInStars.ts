3cdb0602e0c44fcafb655efd7051c77d454f8cd1
Dmitriy Vasilev <raoffonom@icloud.com>
2025-01-09 23:36:36 +0700

    cost

--- src/price/helpers/calculateFinalImageCostInStars.ts
+++ src/price/helpers/calculateFinalImageCostInStars.ts
@@ -1,7 +1,6 @@
 import { starCost, interestRate } from '..';
 
-// Функция для расчета окончательной стоимости изображения в звездах
 export function calculateFinalImageCostInStars(baseCost: number): number {
   const finalCostInDollars = baseCost * (1 + interestRate);
-  return Math.round(finalCostInDollars / starCost);
+  return Math.ceil(finalCostInDollars / starCost);
 }
