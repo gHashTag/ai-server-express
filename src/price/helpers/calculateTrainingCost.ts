62e0b9ca172f107f1895047c63339b89a24e5cb6
Dmitriy Vasilev <raoffonom@icloud.com>
2025-02-04 01:58:25 +0700

    price update

--- src/price/helpers/calculateTrainingCost.ts
+++ src/price/helpers/calculateTrainingCost.ts
@@ -1,10 +1,13 @@
 export const costPerStepInStars = 1.47
 
+const companyInterestRate = 2.0 // Добавляем интерес компании 200%
+
 export function calculateTrainingCostInStars(steps: number): number {
   const totalCostInStars = steps * costPerStepInStars
+  const totalCostWithInterest = totalCostInStars * (1 + companyInterestRate) // Учитываем интерес компании
 
   // Округляем до одного знака после запятой
-  return parseFloat(totalCostInStars.toFixed(2))
+  return parseFloat(totalCostWithInterest.toFixed(2)) // Возвращаем стоимость с учетом интереса
 }
 
 export function calculateTrainingCostInDollars(steps: number): number {
